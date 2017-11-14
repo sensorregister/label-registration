a07d321437bb7e3bc8cdf44cb41c97853b84434a
Raymond Kroon <raymond@k3n.nl>
2017-11-14 10:47:17 +0100

    remove security constraints from /built

--- src/main/java/nl/kadaster/sensor/labelregistration/config/SecurityConfig.java
+++ src/main/java/nl/kadaster/sensor/labelregistration/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-				.antMatchers("/", "/home", "js/**", "css/**")
+				.antMatchers("/", "/home", "/js/**", "/css/**", "/built/**")
 				.permitAll()
 				.anyRequest()
 				.authenticated()
